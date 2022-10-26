67cb8c439e8c83723df528523233f6bc7dd80f30
safwanjmuhammed <safwanjmuhammed@gmail.com>
2022-10-26 12:17:42 +0530

    up

---
+++ lib/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'mainscreen.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("LoginPage",),
+          //centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "LoginPage",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon:  Icon(Icons.account_circle),
+                      label: Text("UserName"),
+                      hintText: "UserName must be your email",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      )),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: TextField(
+                  obscuringCharacter: "*",
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.password) ,
+                      label: Text("Password"),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 20),
+                child: ElevatedButton(
+                    onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MainScreen()));
+                },
+                    child: const Text("LOGIN")),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text("Not a User? Register Here!!!"))
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.green),
+    home: Splash(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Splash extends StatefulWidget{
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  "https://mir-s3-cdn-cf.behance.net/project_modules/disp/15549a14589707.5628669c64769.png")),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ///fetching image from asset way 1
+              Image.asset(
+                "assets/images/food.png",
+                height: 200,
+                width: 200,
+              ),
+              ///fetching image from asset way 2
+              //const Image(image: AssetImage("assets/images/food.png")),
+              const Text("MYAPP",
+                  style: TextStyle(fontSize: 40, color: Colors.orange)),
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
